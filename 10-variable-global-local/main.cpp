7023fd246c28b4824b7939a2d2cc92bd581ac591
JAreina <jareinafdez@gmail.com>
2018-06-19 18:15:58 +0200

    variable global, local

--- 10-variable-global-local/main.cpp
+++ 10-variable-global-local/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std ;
 const double EUR_PTS = 166.386 ; // Declaración de constante GLOBAL
+void ambito();
 
 int main()
 {
@@ -9,4 +10,21 @@ int main()
    cin >> euros ;
    double pesetas = euros * EUR_PTS ; // Declaración de variable LOCAL
    cout << euros << " Euros equivalen a " << pesetas << " Pts" << endl ;
+   
+   ambito();
+}
+
+
+void ambito(){
+	
+
+int x = 3 ;
+int z = x * 2 ; // x es vble de tipo int con valor 3
+			{// BLOQUE
+			double x = 5.0 ;
+			double n = x * 5 ; // x es vble de tipo double con valor 5.0
+			 cout << "local : " << n << endl;
+			}
+int y = z + 4 ; // z es vble de tipo int con valor 6
+cout << "GLOBAL " << y <<endl;
 }
